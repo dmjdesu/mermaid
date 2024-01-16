37339f7f6f2e35c14a0b451143fa4b4324ff32b6
dmjdesu <dmjdesu@gmail.com>
2024-01-16 19:26:26 +0900

    バグ修正

--- src/App.js
+++ src/App.js
@@ -51,37 +51,6 @@ const contentBaseHeight = 50;
 const heightPerItem = 2;
 const contentHeightItem = 30;
 
-function createBaseNode(
-  index,
-  height,
-  width,
-  margin,
-  label,
-  contentWidth,
-  fontSize,
-  fontWeight,
-  borderColor,
-  tempTitleArray,
-  duplicateLineCount
-) {
-  return {
-    id: String(index + 1),
-    position: {
-      x: width * 200 + margin,
-      y:
-        height * 100 +
-        duplicateLineCount * 30 +
-        tempTitleArray.length * contentHeightItem +
-        contentBaseHeight,
-    },
-    style: {
-      fontSize: fontSize,
-      fontWeight: fontWeight,
-      border: borderColor,
-      overflow: "hidden",
-    },
-  };
-}
 function createMultiNode(
   index,
   height,
@@ -96,27 +65,24 @@ function createMultiNode(
   tempTitleArray,
   duplicateLineCount
 ) {
-  const baseNode = createBaseNode(
-    index,
-    height,
-    width,
-    margin,
-    label,
-    contentWidth,
-    fontSize,
-    fontWeight,
-    borderColor,
-    tempTitleArray,
-    duplicateLineCount
-  );
-
   return {
-    ...baseNode,
     multitype: "multi",
+    id: String(index + 1),
+    position: {
+      x: width * contentWidth + margin,
+      y:
+        height * 100 +
+        duplicateLineCount * 30 +
+        tempTitleArray.length * contentHeightItem +
+        contentBaseHeight,
+    },
     data: { label: label },
     style: {
-      ...baseNode.style,
       width: tempWidth,
+      fontSize: fontSize,
+      fontWeight: fontWeight,
+      border: borderColor,
+      overflow: "hidden",
     },
   };
 }
@@ -134,27 +100,24 @@ function createOriginalNode(
   tempTitleArray,
   duplicateLineCount
 ) {
-  const baseNode = createBaseNode(
-    index,
-    height,
-    width,
-    margin,
-    label,
-    contentWidth,
-    fontSize,
-    fontWeight,
-    borderColor,
-    tempTitleArray,
-    duplicateLineCount
-  );
-
   return {
-    ...baseNode,
     multitype: "single",
+    id: String(index + 1),
+    position: {
+      x: width * 200 + margin,
+      y:
+        height * 100 +
+        duplicateLineCount * 30 +
+        tempTitleArray.length * contentHeightItem +
+        contentBaseHeight,
+    },
     data: { label: label },
     style: {
-      ...baseNode.style,
       width: contentWidth,
+      fontSize: fontSize,
+      fontWeight: fontWeight,
+      border: borderColor,
+      overflow: "hidden",
       whiteSpace: "nowrap",
       textOverflow: "ellipsis",
     },
@@ -175,31 +138,23 @@ function createTitleNode(
   tempTitleArray,
   duplicateLineCount
 ) {
-  const baseNode = createBaseNode(
-    index,
-    height,
-    width,
-    margin,
-    title,
-    body,
-    contentWidth,
-    fontSize,
-    fontWeight,
-    borderColor,
-    tempTitleArray,
-    duplicateLineCount
-  );
-
   return {
-    ...baseNode,
     multitype: "single",
+    id: String(index + 1),
+    position: {
+      x: width * 200 + margin,
+      y:
+        height * 100 +
+        duplicateLineCount * 30 +
+        tempTitleArray.length * contentHeightItem +
+        contentBaseHeight,
+    },
     type: "textUpdater",
     data: {
       title: title,
       body: body,
     },
     style: {
-      ...baseNode.style,
       width: 800,
       fontSize: "15px",
       fontWeight: 700,
@@ -481,8 +436,20 @@ function App() {
           // 一致した場合の処理
           const newText = line.replace(multiPattern, "");
           multiArray.push(newText);
+          if (lines.length === index + 1) {
+            nodeesArray.push({
+              type: "array",
+              id: String(index),
+              data: multiArray,
+            });
+            multiArray = [];
+          }
         } else {
-          if (multiArray.length > 0 && multiArray.length < 7) {
+          if (
+            (multiArray.length > 0 && multiArray.length < 7) ||
+            lines.length === index + 1
+          ) {
+            console.log("push");
             nodeesArray.push({
               type: "array",
               id: String(index),
@@ -646,7 +613,7 @@ function App() {
     let edgesIndex = 1;
 
     tempNodes.forEach((node, index) => {
-      if (tempNodes.length !== index + 1) {
+      if (tempNodes.length >= index + 1) {
         if (node.multitype === "single") {
           tempEdges.push({
             id: "l1-" + edgesIndex,
@@ -663,7 +630,7 @@ function App() {
           });
           edgesIndex += 1;
           let tempIndex = index;
-          while (true) {
+          while (tempIndex + 1 < tempNodes.length) {
             if (tempNodes[tempIndex + 1]["multitype"] === "multi") {
               tempEdges.push({
                 id: "l1-" + edgesIndex,
@@ -697,6 +664,11 @@ function App() {
               "label"
             ].replace(/\(title:[^)]*\)/, ""); // 全て除去
             tempNodes[index]["data"]["title"] = extractedText;
+          }
+
+          if (tempNodes.length === index + 1) {
+            setEdges(tempEdges);
+            return;
           }
           if (tempNodes[index + 1]["multitype"] === "multi") {
             if (vsPattern.test(tempNodes[index]["data"]["label"])) {
@@ -757,8 +729,10 @@ function App() {
               edgesIndex += 1;
             }
           }
+          console.log(index);
+          console.log(tempNodes.length);
           let tempIndex = index;
-          while (true) {
+          while (tempIndex + 1 < tempNodes.length - 1) {
             if (tempNodes[tempIndex + 1]["multitype"] === "single") {
               tempEdges.push({
                 id: "l1-" + edgesIndex,
@@ -792,6 +766,10 @@ function App() {
         }
       }
     });
+    console.log("tempNodes");
+    console.log(tempNodes);
+    console.log("tempEdges");
+    console.log(tempEdges);
     setEdges(tempEdges);
   };
 
